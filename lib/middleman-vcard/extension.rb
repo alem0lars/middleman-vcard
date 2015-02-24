2a7310996e9f92f13081298f7cec49f146198352
Alessandro Molari <molari.alessandro@gmail.com>
2015-02-24 17:42:27 +0100

    Added destination path option to middleman extension

--- lib/middleman-vcard/extension.rb
+++ lib/middleman-vcard/extension.rb
@@ -3,25 +3,30 @@ module MiddlemanVCard
   # A Middleman extension to generate VCards.
   #
   class VCardExtension < Middleman::Extension
-    option :name,           "The VCard name", required: true
+
+    option :name,      nil, "The VCard name" # TODO: It should be required.
     option :emails,    [],  "The VCard emails"
     option :phones,    [],  "The VCard telephones"
     option :addresses, [],  "The VCard addresses"
     option :photo,     nil, "The VCard photo"
 
+    option :destination_path, nil, "Destination path for the generated VCard"
+
     def initialize(app, options_hash={}, &block)
       super
 
-      @vcard_generator = ::Generator.new(options_hash[:name],
-                                         options_hash[:emails],
-                                         options_hash[:phones],
-                                         options_hash[:addresses],
-                                         options_hash[:photo],
-                                         logger)
+      @destination_path = options_hash[:destination_path] ||
+          File.join(app.root, app.config.source, "#{options_hash[:name]}.vcf")
+      @vcard_generator  = ::Generator.new(options_hash[:name],
+                                          options_hash[:emails],
+                                          options_hash[:phones],
+                                          options_hash[:addresses],
+                                          options_hash[:photo],
+                                          logger)
     end
 
     def after_configuration
-      @vcard_generator.generate
+      @vcard_generator.generate(@destination_path)
     end
 
   end
