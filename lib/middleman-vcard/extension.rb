d14804918b04fe3abafcf91e42cb4b45f16c2bca
Alessandro Molari <molari.alessandro@gmail.com>
2015-02-24 17:53:23 +0100

    Fix extension namespace

--- lib/middleman-vcard/extension.rb
+++ lib/middleman-vcard/extension.rb
@@ -1,36 +1,40 @@
-module MiddlemanVCard
-  ##
-  # A Middleman extension to generate VCards.
-  #
-  class VCardExtension < Middleman::Extension
+module Middleman
+  module VCard
+    ##
+    # A Middleman extension to generate VCards.
+    #
+    class VCardExtension < Middleman::Extension
 
-    option :name,      nil, "The VCard name" # TODO: It should be required.
-    option :emails,    [],  "The VCard emails"
-    option :phones,    [],  "The VCard telephones"
-    option :addresses, [],  "The VCard addresses"
-    option :photo,     nil, "The VCard photo"
+      option :name,      nil, "The VCard name" # TODO: It should be required.
+      option :emails,    [],  "The VCard emails"
+      option :phones,    [],  "The VCard telephones"
+      option :addresses, [],  "The VCard addresses"
+      option :photo,     nil, "The VCard photo"
 
-    option :destination_path, nil, "Destination path for the generated VCard"
+      option :destination_path, nil, "Destination path for the generated VCard"
 
-    def initialize(app, options_hash={}, &block)
-      super
+      def initialize(app, options_hash={}, &block)
+        super
 
-      @destination_path = options_hash[:destination_path] ||
-          File.join(app.root, app.config.source, "#{options_hash[:name]}.vcf")
-      @vcard_generator  = ::Generator.new(options_hash[:name],
-                                          options_hash[:emails],
-                                          options_hash[:phones],
-                                          options_hash[:addresses],
-                                          options_hash[:photo],
-                                          logger)
+        @destination_path = options_hash[:destination_path] ||
+            File.join(app.root, app.config.source, "#{options_hash[:name]}.vcf")
+        @vcard_generator  = ::Generator.new(options_hash[:name],
+                                            options_hash[:emails],
+                                            options_hash[:phones],
+                                            options_hash[:addresses],
+                                            options_hash[:photo],
+                                            logger)
+      end
+
+      def after_configuration
+        @vcard_generator.generate(@destination_path)
+      end
+
     end
 
-    def after_configuration
-      @vcard_generator.generate(@destination_path)
-    end
+    alias :included :registered
 
   end
-
 end
 
-::Middleman::Extensions.register(:vcard, MiddlemanVCard::VCardExtension)
+::Middleman::Extensions.register(:vcard, Middleman::VCard::VCardExtension)
