7617ab5c103e96c7c3e2bac04dcdd9a3067b111a
Alessandro Molari <molari.alessandro@gmail.com>
2015-02-25 09:05:42 +0100

    Added vcard_link helper; destination_path -> dst_path

--- lib/middleman-vcard/extension.rb
+++ lib/middleman-vcard/extension.rb
@@ -1,3 +1,6 @@
+require "pathname"
+
+
 module Middleman
   module VCard
     ##
@@ -5,19 +8,28 @@ module Middleman
     #
     class VCardExtension < Middleman::Extension
 
-      option :name,      nil, "The VCard name" # TODO: It should be required.
+      option :name,      nil, "The VCard name", required: true
       option :emails,    [],  "The VCard emails"
       option :phones,    [],  "The VCard telephones"
       option :addresses, [],  "The VCard addresses"
       option :photo,     nil, "The VCard photo"
 
-      option :destination_path, nil, "Destination path for the generated VCard"
+      option :dst_path, nil, "The destination path for the generated VCard",
+             required: true
 
       def initialize(app, options_hash={}, &block)
         super
 
-        @destination_path = options_hash[:destination_path] ||
-            File.join(app.root, app.config.source, "#{options_hash[:name]}.vcf")
+        @name = options_hash[:name]
+
+        if options_hash[:dst_path]
+          @base_dir_path   = Pathname.new(File.dirname(options_hash[:dst_path]))
+          @vcard_file_name = File.basename(options_hash[:dst_path])
+        else
+          @base_dir_path   = Pathname.new(app.root).join(app.config.source)
+          @vcard_file_name = "#{@name}.vcf"
+        end
+
         @vcard_generator  = Middleman::VCard::Generator.new(
             options_hash[:name], options_hash[:emails],
             options_hash[:phones], options_hash[:addresses],
@@ -25,7 +37,18 @@ module Middleman
       end
 
       def after_configuration
-        @vcard_generator.generate(@destination_path)
+        @vcard_generator.generate(@base_dir_path.join(@vcard_file_name))
+      end
+
+      helpers do
+
+        def vcard_link(title=nil, **kwargs)
+          prefix = @base_dir_path.relative_path_from(
+              Pathname.new(app.root, app.config.source))
+
+          link_to(title || @name, "#{prefix}/#{@vcard_file_name}", kwargs)
+        end
+
       end
 
     end
